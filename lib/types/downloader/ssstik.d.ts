1c4f2d6743d0d522b97f347e63ecce0fd7a9c0f3
mongy910 <matthew910@gmail.com>
2024-12-19 15:22:08 -0800

    Add lib folder

---
+++ lib/types/downloader/ssstik.d.ts
@@ -0,0 +1,28 @@
+export type SSSTikFetchTT = {
+    status: "success" | "error";
+    message?: string;
+    result?: string;
+};
+export type SSSTikResponse = {
+    status: "success" | "error";
+    message?: string;
+    result?: {
+        type: "image" | "video" | "music";
+        desc?: string;
+        author?: Author;
+        statistics?: Statistics;
+        images?: string[];
+        video?: string;
+        music?: string;
+        direct?: string;
+    };
+};
+export type Author = {
+    avatar: string;
+    nickname: string;
+};
+export type Statistics = {
+    likeCount: string;
+    commentCount: string;
+    shareCount: string;
+};
